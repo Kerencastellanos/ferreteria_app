e893ba7c1c40f32d1486548ec5ca5bde804d88a9
robert-virtual <robertocastill63@gmail.com>
2022-03-18 10:21:31 -0600

    comentarios de typos de datos😎

--- pages/HistorialCompras.jsx
+++ pages/HistorialCompras.jsx
@@ -1,16 +1,35 @@
+/**
+ * @typedef {{ detalles:Detalle[] }} Venta
+ * @typedef {{ cantidad:number,precio:number,producto:Producto }} Detalle
+ * @typedef {{ imagenes:Imagen[],nombre:string }} Producto
+ * @typedef {{ url:string }} Imagen
+ * @typedef {{ data:{ventas:Venta[]} }} Res
+ */
+
 import { FlatList, Text, StyleSheet, View } from "react-native";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ListaProductos } from "../components";
 
 export function HistorialCompras() {
+  /**
+   * @type [ventas:Venta[]]
+   */
   const [ventas, setVentas] = useState([]);
   const [prods, setProds] = useState([]);
   useEffect(() => {
     (async () => {
-      const { data } = await axios.get("/ventas");
+      /**
+       * @type Res
+       */
+      let { data } = await axios.get("/ventas");
       setVentas(data.ventas);
-      setProds(data.ventas.map((v) => ({ cantidad: v.cantidad })));
+      setProds(
+        data.ventas.map((v, i) => ({
+          cantidad: v.detalles[i].cantidad,
+          images: v.image,
+        }))
+      );
       console.log("historial: ", data.ventas);
     })();
   }, []);
